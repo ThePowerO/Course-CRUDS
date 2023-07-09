8e49d24881def041b8bf58f60fa3f30a6bf3a316
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-09 17:35:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ try:
         )""")
         print("La table des classes a été créée.")
 
-except sqlite3.Error as erro:
+except sqlite3.Error as erreur:
     print("La création de la table a mal tourné: ", erro)
 
 try:
