e30aa4d0e43d5a2d30ec38ff55cec3b45fc2879c
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-10 17:16:32 -0300

    Update window.py

--- window.py
+++ window.py
@@ -36,3 +36,4 @@ frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
 ttk.Separator(fenêtre, orient=HORIZONTAL).grid(row=1, columnspan=1, ipadx=680)
 
 frame_données = Frame(fenêtre, width=850, height=62, background=co1)
+frame_données.grid(row=2, column=0, pady=0, padx=0, sticky=NSEW)
