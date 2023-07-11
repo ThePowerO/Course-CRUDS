c35a026953085cb2e0e4581af95e52a1a2830191
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-11 17:20:43 -0300

    Update window.py

--- window.py
+++ window.py
@@ -6,6 +6,7 @@ from tkinter import filedialog as fd
 
 from PIL import ImageTk, Image
 
+#Couleurs
 co0 = "#2e2d2b"  # Noir
 co1 = "#feffff"  # Blanc 
 co2 = "#e5e5e5"  # grey
@@ -27,6 +28,7 @@ fenêtre.resizable(width=FALSE, height=FALSE)
 Style = Style(fenêtre)
 Style.theme_use('clam')
 
+#Création des Frames
 frame_logo = Frame(fenêtre, width=850, height=52, background=co6)
 frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
 
