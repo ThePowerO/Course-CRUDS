898e3e0e17521e6b00328db6cedcd800dc0e4956
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-12 18:15:05 -0300

    Update window.py

--- window.py
+++ window.py
@@ -111,6 +111,7 @@ def ajouter():
         tree_curso.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
         tree_curso.grid(column=0, row=1, sticky='nsew')
         vsb.grid(column=1, row=1, sticky='ns')
+        hsb.grid(column=0, row=2, sticky='ew')
 
 
 def sauver():
