f5a94610ee05bb522e5630bb6885115d6892b327
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-14 19:12:16 -0300

    Update window.py

--- window.py
+++ window.py
@@ -161,6 +161,7 @@ def ajouter():
     c_cours.place(x=407, y=100)
 
     l_date_initiale = Label(frame_détails, text="Date initiale:", font=("Ivy 10"), anchor=NW,height=1, bg=co1, fg=co4)
+    l_date_initiale.place(x=406, y=130)
 
 
 def sauver():
