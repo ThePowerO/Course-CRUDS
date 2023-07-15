bc5fc3555e5dbb6e0d9c989780a52f8eeb9da92d
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-15 16:17:05 -0300

    Update window.py

--- window.py
+++ window.py
@@ -57,6 +57,7 @@ frame_logo.place(x=0, y=0)
 def étudiants():
     
     l_nom = Label(frame_détails, text="Nom:", font=("Ivy 10"), anchor=NW,height=1, bg=co1, fg=co4)
+    l_nom.place(x=4, y=10)
 
     
 def ajouter():
