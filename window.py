a1ce80d7927a178686083ef497714d37faa1e486
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-16 16:12:57 -0300

    Update window.py

--- window.py
+++ window.py
@@ -136,6 +136,8 @@ def étudiants():
     bouton_update2 = Button(frame_détails, anchor=CENTER, text="Update".upper(), width=9, overrelief=RIDGE, font=("Ivy 7 bold"), bg=co6, fg=co1,)
     bouton_update2.place(x=627, y=135)
 
+    bouton_supprimer2 = Button(frame_détails, anchor=CENTER, text="Supprimer".upper(), width=9, overrelief=RIDGE, font=("Ivy 7 bold"), bg=co7, fg=co1,)
+
 
 def ajouter():
     # Création des Frames Cours et Tableaus
