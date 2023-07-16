a0f7229dc9cb496efe906ccc10dcc1b6d1c59608
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-16 16:10:18 -0300

    Update window.py

--- window.py
+++ window.py
@@ -120,6 +120,7 @@ def étudiants():
     l_ligne = Label(frame_détails, text="h", relief=GROOVE, height=100, font=("Ivy 1"), bg=co0, fg=co0, anchor=NW)
     l_ligne.place(x=610, y=10)
     l_ligne = Label(frame_détails, text="h", relief=GROOVE, height=100, font=("Ivy 1"), bg=co1, fg=co0, anchor=NW)
+    l_ligne.place(x=608, y=10)
 
 
 def ajouter():
