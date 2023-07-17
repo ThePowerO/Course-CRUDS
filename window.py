2630e89da2955ee380a3841cc96668dadf8a54b9
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-17 18:32:36 -0300

    Update window.py

--- window.py
+++ window.py
@@ -249,6 +249,7 @@ def ajouter():
                 bouton_sauver1.destroy()
 
             bouton_sauver1 = Button(frame_détails, command=update, anchor=CENTER, text="Sauver".upper(), width=10, overrelief=RIDGE, font=("Ivy 7"), bg=co3, fg=co1)
+            bouton_sauver1.place(x=227, y=130) 
 
 
 
