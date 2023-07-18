a1d4caf2a74f299ba0306ef3caa62169a8bef830
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-18 15:24:02 -0300

    Update window.py

--- window.py
+++ window.py
@@ -392,6 +392,7 @@ def ajouter():
 
                 liste = [nom, cours, date, valeur_id]
 
+                for i in liste:
 
     l_nom = Label(frame_détails, text="Nom du Classes:", font=("Ivy 10"), anchor=NW,height=1, bg=co1, fg=co4)
     l_nom.place(x=404, y=10)
