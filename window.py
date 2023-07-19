ce74f14b2461ad522e9450b7317e4f00d3624503
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-19 17:21:57 -0300

    Update window.py

--- window.py
+++ window.py
@@ -89,12 +89,6 @@ def étudiants():
         c_classes.delete(0,END)
 
         montrer_étudiant()
-
-
-
-
-
-
     
     l_nom = Label(frame_détails, text="Nom:", font=("Ivy 10"), anchor=NW,height=1, bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
@@ -170,7 +164,7 @@ def étudiants():
     bouton_rechercher = Button(frame_détails, anchor=CENTER, text="Rechercher", width=9, overrelief=RIDGE, font=("Ivy 7 bold"), bg=co2, fg=co0)
     bouton_rechercher.place(x=757, y=35)
 
-    bouton_sauver2 = Button(frame_détails, anchor=CENTER, text="Sauver".upper(), width=9, overrelief=RIDGE, font=("Ivy 7 bold"), bg=co3, fg=co1)
+    bouton_sauver2 = Button(frame_détails, command=noveau_étudiants, anchor=CENTER, text="Sauver".upper(), width=9, overrelief=RIDGE, font=("Ivy 7 bold"), bg=co3, fg=co1)
     bouton_sauver2.place(x=627, y=110) 
 
     bouton_update2 = Button(frame_détails, anchor=CENTER, text="Update".upper(), width=9, overrelief=RIDGE, font=("Ivy 7 bold"), bg=co6, fg=co1,)
