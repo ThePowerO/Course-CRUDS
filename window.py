cfb74cb1a99b0324b0214b288a2b943c801aa46c
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-19 17:52:44 -0300

    Update window.py

--- window.py
+++ window.py
@@ -82,6 +82,7 @@ def étudiants():
 
         messagebox.showinfo("Succès", "Tu rempli tout avec succès")
 
+        #Nettoyage des entrées
         e_nom.delete(0,END)
         e_email.delete(0,END)
         c_sexe.delete(0,END)
