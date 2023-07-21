8f0a0128faa8fa91b15cbed755fc2dc60a9d507a
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-20 21:41:41 -0300

    Update window.py

--- window.py
+++ window.py
@@ -125,6 +125,7 @@ def étudiants():
             image = Image.open(image)
             image = image.resize((130,130))
             image = ImageTk.PhotoImage(image)
+            l_image = Label(frame_détails, image=image, bg=co1, fg=co4)
 
 
     #Création d'Entrées
