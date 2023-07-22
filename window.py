aa0298b9a0cfe1bb31fbea0b89476676429de9e1
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-21 21:00:51 -0300

    Update window.py

--- window.py
+++ window.py
@@ -184,6 +184,8 @@ def étudiants():
 
             montrer_étudiant()
 
+        except IndexError:
+
 
     #Création d'Entrées
     l_nom = Label(frame_détails, text="Nom:", font=("Ivy 10"), anchor=NW,height=1, bg=co1, fg=co4)
